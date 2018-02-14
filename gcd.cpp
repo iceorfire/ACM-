81f3575822ff77b372b884371014416681f60135
allen.peng <1057836780@qq.com>
2018-02-14 15:19:47 +0800

    增加了min,max

--- gcd.cpp
+++ gcd.cpp
@@ -1,11 +1,14 @@
 #include<iostream>
 using namespace std;
+int min(int a,int b){return a<b?a:b;}
+int max(int a,int b){return a>b?a:b;}
 int gcd(int a,int b){return b==0?a:gcd(b,a%b);}
 
 int main()
 {
 	int a,b;
 	cin>>a>>b;
+	cout<<min(a,b)<<" "<<max(a,b)<<endl;
 	cout<<gcd(a,b)<<endl;
 	return 0;
 }
